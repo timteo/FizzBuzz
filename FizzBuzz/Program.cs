fd5ebe66933b7fba3cbfba003440387d6bcb444d
timteo <timothy.teo@titansoft.com.sg>
2016-07-15 12:22:22 +0800

    initial

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace FizzBuzz
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                sb.AppendLine(print(i));
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+
+        public static string print(int number)
+        {
+            if (number%15 == 0)
+            {
+                return "FizzBuzz";
+            }
+            if (number%3 == 0)
+            {
+                return "Fizz";
+            }
+            if (number%5 == 0)
+            {
+                return "Buzz";
+            }
+
+            return number.ToString();
+        }
+    }
+}
